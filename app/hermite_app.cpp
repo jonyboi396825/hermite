9254764ada974e38419f9aac3c6a8c6912ed2df7
Jonathan Liu <jonathanhliu21@gmail.com>
2023-05-23 17:17:54 -0700

    finish example

--- app/hermite_app.cpp
+++ app/hermite_app.cpp
@@ -1,18 +1,18 @@
+#include <cstddef>
 #include <iostream>
 
 #include <hermite/hermite.hpp>
-
-namespace spl = hermite;
+#include <hermite/cubic.hpp>
 
 int main() {
   // create hermite object
-  spl::Hermite<1> h;
+  hermite::Hermite<1> h;
 
   // create poses
-  spl::Pose<1> p0{-3, {-2}, {0}};
-  spl::Pose<1> p1{0, {2}, {1}};
-  spl::Pose<1> p2{2, {3}, {2}};
-  spl::Pose<1> p3{6, {0}, {0}};
+  hermite::Pose<1> p0{0, {1}, {2}};
+  hermite::Pose<1> p1{2, {2}, {0}};
+  hermite::Pose<1> p2{5, {0}, {0}};
+  hermite::Pose<1> p3{8, {0}, {1}};
 
   // add poses into hermite object
   h.insert(p0);
@@ -20,18 +20,29 @@ int main() {
   h.insert(p2);
   h.insert(p3);
 
-  // calculate position
-  auto pos1 = h.getPos(-1.5); // <-0.375>
-  auto pos2 = h.getPos(3.5);  // <3.223>
-  auto vel1 = h.getVel(-1.5); // <-0.375>
-  auto vel2 = h.getVel(3.5);  // <3.223>
-  auto acc1 = h.getAcc(-1.5); // <-0.375>
-  auto acc2 = h.getAcc(3.5);  // <3.223>
+  // create cubic object
+  hermite::Cubic<1> cub{h.getAllWaypoints()};
 
-  std::cout << pos1.toString() << std::endl;
-  std::cout << pos2.toString() << std::endl;
-  std::cout << vel1.toString() << std::endl;
-  std::cout << vel2.toString() << std::endl;
-  std::cout << acc1.toString() << std::endl;
-  std::cout << acc2.toString() << std::endl;
+  // calculate position vectors from cubic
+  svector::Vector<1> pos1 = cub.getPos(1);
+  svector::Vector<1> pos2 = cub.getPos(4);
+  svector::Vector<1> vel1 = cub.getVel(1);
+  svector::Vector<1> vel2 = cub.getVel(4);
+  svector::Vector<1> acc1 = cub.getAcc(1);
+  svector::Vector<1> acc2 = cub.getAcc(4);
+
+  std::cout << pos1.toString() << std::endl; // <2.105>
+  std::cout << pos2.toString() << std::endl; // <0.712>
+  std::cout << vel1.toString() << std::endl; // <0.355>
+  std::cout << vel2.toString() << std::endl; // <-0.749>
+  std::cout << acc1.toString() << std::endl; // <-1.211>
+  std::cout << acc2.toString() << std::endl; // <0.015>
+
+  // looping through positions
+  double curTime = cub.getLowestTime();
+  double timestep = 0.01;
+  while (curTime <= cub.getHighestTime()) {
+    std::cout << cub.getPos(curTime).toString() << std::endl;
+    curTime += timestep; 
+  }
 }
