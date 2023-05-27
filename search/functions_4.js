7bef411afd4922ab0436f28a355ac53008ae81c6
jonyboi396825 <81734282+jonyboi396825@users.noreply.github.com>
2023-05-27 21:13:01 +0000

    Deploying to gh-pages from @ jonyboi396825/hermite@5d0ce8a6c16d9ba48cdc3cad2749c6c33cf019be 🚀

--- search/functions_4.js
+++ search/functions_4.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['end_151',['end',['../a00102.html#a90b984cf25cbc2043736a5673f03444a',1,'svector::Vector::end() noexcept'],['../a00102.html#a4efdfe65e424daca281d09d3341ab8d9',1,'svector::Vector::end() const noexcept']]],
   ['erase_152',['erase',['../a00094.html#a989d6eb21783af0472c2eec90a45e3d0',1,'hermite::Hermite::erase(const Pose&lt; D &gt; &amp;waypoint)'],['../a00094.html#afba68186501dfe74380a898c65f246da',1,'hermite::Hermite::erase(const double time)']]],
-  ['exists_153',['exists',['../a00094.html#a81df75113de2944b61fcc89d94c6707f',1,'hermite::Hermite::exists(const Pose&lt; D &gt; &amp;waypoint)'],['../a00094.html#aee7697c99fe9c1feebe187edbd5d44e6',1,'hermite::Hermite::exists(const double time)']]]
+  ['exists_153',['exists',['../a00094.html#a52e6d29de2903a53347a15ac2093dd2c',1,'hermite::Hermite::exists(const Pose&lt; D &gt; &amp;waypoint) const'],['../a00094.html#a8b4b6ac98c4e39b6083e5422f6b226b4',1,'hermite::Hermite::exists(const double time) const']]]
 ];
